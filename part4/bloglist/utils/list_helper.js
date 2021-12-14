3f6a219854d17dbe0f557f33efe2f6e959e2d4c8
davidiglesiasgomez <david.iglesias@gmail.com>
2021-12-14 20:12:24 +0100

    Part4 - helper functions and unit test - step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => {return total+blog.likes}, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {}
+  let blog = blogs.reduce(function(previousBlog, currentBlog) {
+    if (previousBlog.likes > currentBlog.likes) {
+      return previousBlog
+    } else {
+      return currentBlog
+    }
+  })
+  return {
+    'title': blog.title,
+    'author': blog.author,
+    'likes': blog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
