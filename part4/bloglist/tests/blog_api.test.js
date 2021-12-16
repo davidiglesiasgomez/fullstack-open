a550f1ca0f0dcdb731ad76e1caa072cb61ed8599
davidiglesiasgomez <david.iglesias@gmail.com>
2021-12-16 23:07:04 +0100

    Part4 - blog list tests - step6 - refactoring test

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -3,158 +3,104 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: '5a422bc61b54a676234d17fc',
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
-    __v: 0
-  }
-]
+describe('when there is initially some blogs saved', () => {
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[2])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[3])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[4])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[5])
-  await blogObject.save()
-})
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initialBlogs.length)
-})
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-  const titles = response.body.map(r => r.title)
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
 
-  expect(titles).toContain('TDD harms architecture')
-})
+    expect(titles).toContain('TDD harms architecture')
+  })
 
-test('the unique identifier property of the blog posts is named id', async () => {
-  const response = await api.get('/api/blogs')
+  test('the unique identifier property of the blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
 
-  expect(response.body[0].id).toBeDefined()
-})
+    expect(response.body[0].id).toBeDefined()
+  })
 
-test('new blog can be added', async () => {
-  const newBlog = {
-    'title': 'Test blog',
-    'author': 'Unknown',
-    'url': 'foo.bar.com',
-    'likes': 0
-  }
+  describe('viewing a specific blog', () => {
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+  describe('addition of a new blog', () => {
 
-  const response = await api.get('/api/blogs')
+    test('new blog can be added', async () => {
+      const newBlog = {
+        'title': 'Test blog',
+        'author': 'Unknown',
+        'url': 'foo.bar.com',
+        'likes': 0
+      }
 
-  const titles = response.body.map(r => r.title)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  expect(response.body).toHaveLength(initialBlogs.length + 1)
-  expect(titles).toContain(
-    'Test blog'
-  )
-})
+      const response = await api.get('/api/blogs')
 
-test('new blog without likes at the request defaults to zero', async () => {
-  const newBlog = {
-    'title': 'Test blog',
-    'author': 'Unknown',
-    'url': 'foo.bar.com'
-  }
+      const titles = response.body.map(r => r.title)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+      expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+      expect(titles).toContain(
+        'Test blog'
+      )
+    })
 
-  const response = await api.get('/api/blogs')
+    test('new blog without likes at the request defaults to zero', async () => {
+      const newBlog = {
+        'title': 'Test blog',
+        'author': 'Unknown',
+        'url': 'foo.bar.com'
+      }
 
-  const blog = response.body.find(r => r.title === newBlog.title)
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  expect(blog.likes).toBeDefined()
-  expect(blog.likes).toBe(0)
-})
+      const response = await api.get('/api/blogs')
 
-afterAll(async () => {
-  await mongoose.connection.close()
-})
+      const blog = response.body.find(r => r.title === newBlog.title)
 
-test('new blog without title and url at the request returns status code 400 Bad Request', async () => {
-  const newBlog = {
-    'author': 'Unknown',
-  }
+      expect(blog.likes).toBeDefined()
+      expect(blog.likes).toBe(0)
+    })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    test('new blog without title and url at the request returns status code 400 Bad Request', async () => {
+      const newBlog = {
+        'author': 'Unknown',
+      }
+
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+    })
+
+  })
+
 })
 
 afterAll(async () => {
