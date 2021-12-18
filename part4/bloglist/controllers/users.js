da779d40aec6153fefa461f3d45d9338a6be2755
davidiglesiasgomez <david.iglesias@gmail.com>
2021-12-18 11:27:27 +0100

    Part4 - bloglist expansion - step4 - username and password at least 3 characters long

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -5,6 +5,14 @@ const bcrypt = require('bcrypt')
 usersRouter.post('/', async (request, response) => {
   const body = request.body
 
+  if (body.username.length < 3) {
+    return response.status(400).json({error: '`username` must be at least 3 characters long'})
+  }
+
+  if (body.password.length < 3) {
+    return response.status(400).json({error: '`password` must be at least 3 characters long'})
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
