4b01f0857475a475268e9fc3d3f1fcb4de369b2c
David <david.iglesias@gmail.com>
2021-12-20 14:54:30 +0100

    Mongoose collection.ensureIndex deprecation warning use "useCreateIndex" true

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -14,7 +14,7 @@ logger.info('process.env.NODE_ENV', process.env.NODE_ENV)
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => {
     logger.info('connected to MongoDB')
   })
