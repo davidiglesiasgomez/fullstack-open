3cce7c7c5f93bfffd133450c03d4feba23da6235
David <david.iglesias@gmail.com>
2021-12-22 13:47:49 +0100

    Part5 - bloglist frontend - step7 - show/hide blog info

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisible = (event) => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <strong>{blog.title}</strong> <button onClick={toggleVisible}>{ visible ? 'hide' : 'show' }</button>
+      { visible &&
+      <>
+        <br />{blog.url}
+        <br />{blog.likes} <button>like</button>
+        <br />{blog.author}
+      </>
+      }
+    </div>
+  )
+}
 
 export default Blog
