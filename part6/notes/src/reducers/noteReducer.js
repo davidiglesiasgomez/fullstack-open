6e7720e63e1730067d03d69d433043204982e0c6
davidiglesiasgomez <david.iglesias@gmail.com>
2022-01-30 09:07:47 +0100

    Part6 - notes - retrieve from index.js

--- part6/notes/src/reducers/noteReducer.js
+++ part6/notes/src/reducers/noteReducer.js
@@ -1,17 +1,4 @@
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
-
-const noteReducer = (state = initialState, action) => {
+const noteReducer = (state = [], action) => {
   if (action.type === 'NEW_NOTE') {
     return [...state, action.data]
   }
