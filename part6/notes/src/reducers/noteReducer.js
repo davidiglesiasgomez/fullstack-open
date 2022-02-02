819c98b2658720d76e3565f00df08dae4fdf08da
davidiglesiasgomez <david.iglesias@gmail.com>
2022-02-02 19:35:32 +0100

    Part6 - notes - redux middleware

--- part6/notes/src/reducers/noteReducer.js
+++ part6/notes/src/reducers/noteReducer.js
@@ -1,3 +1,5 @@
+import noteService from '../services/notes'
+
 const noteReducer = (state = [], action) => {
   if (action.type === 'NEW_NOTE') {
     return [...state, action.data]
@@ -21,10 +23,13 @@ const noteReducer = (state = [], action) => {
   return state
 }
 
-export const createNote = (data) => {
-  return {
-    type: 'NEW_NOTE',
-    data
+export const createNote = content => {
+  return async dispatch => {
+    const newNote = await noteService.createNew(content)
+    dispatch({
+      type: 'NEW_NOTE',
+      data: newNote,
+    })
   }
 }
 
@@ -35,10 +40,13 @@ export const toggleImportanceOf = (id) => {
   }
 }
 
-export const initializeNotes = (notes) => {
-  return {
-    type: 'INIT_NOTES',
-    data: notes,
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch({
+      type: 'INIT_NOTES',
+      data: notes,
+    })
   }
 }
 
