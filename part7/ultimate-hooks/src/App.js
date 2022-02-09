ec4ace5f32d5c22e02be3d8ffd455918850ee212
David <david.iglesias@gmail.com>
2022-02-09 08:47:34 +0100

    Part7 - ultimate hooks - add new item in hook

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -28,8 +28,10 @@ const useResource = (baseUrl) => {
     })
   }, [baseUrl])
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+    return response.data
   }
 
   const service = {
