ebd8ad82f4d0f33f789c517e6092999f2eb0a63e
davidiglesiasgomez <david.iglesias@gmail.com>
2022-02-28 23:13:55 +0100

    Part8 - library frontend - exercise12 - authors birth year advanced

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -34,7 +34,7 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
-      <UpdateBirthYear />
+      <UpdateBirthYear authors={authors} />
     </div>
   )
 }
--- part8/library-frontend/src/components/UpdateBirthYear.js
+++ part8/library-frontend/src/components/UpdateBirthYear.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { useMutation } from '@apollo/client'
 import { EDIT_AUTHOR, ALL_AUTHORS } from '../queries'
 
-const UpdateBirthYear = () => {
+const UpdateBirthYear = ({ authors }) => {
   const [name, setName] = useState('')
   const [born, setBorn] = useState('')
 
@@ -28,20 +28,22 @@ const UpdateBirthYear = () => {
     <h3>Set birthyear</h3>
     <form onSubmit={submit}>
       <div>
+        <label>
           name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
-        </div>
-        <div>
-          born
-          <input
-            value={born}
-            onChange={({ target }) => setBorn(target.value)}
-          />
-        </div>
-        <button type="submit">update author</button>
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            <option value="">-- select one --</option>
+            {authors.map(author => <option value={author.name}>{author.name}</option>)}
+          </select>
+        </label>
+      </div>
+      <div>
+        born
+        <input
+          value={born}
+          onChange={({ target }) => setBorn(target.value)}
+        />
+      </div>
+      <button type="submit">update author</button>
     </form>
   </>
   )
