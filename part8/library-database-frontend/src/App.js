ee75f0e0e845bdd7e7b8f6db6dcbd443c5cec340
davidiglesiasgomez <david.iglesias@gmail.com>
2022-03-07 23:13:01 +0100

    Part8 - library database frontend - exercise24 - subscription base

--- part8/library-database-frontend/src/App.js
+++ part8/library-database-frontend/src/App.js
@@ -1,15 +1,22 @@
 import { useState, useEffect } from 'react'
-import { useApolloClient } from '@apollo/client'
+import { useSubscription, useApolloClient } from '@apollo/client'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import Recommend from './components/Recommend'
+import { BOOK_ADDED } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
   const client = useApolloClient()
+
+  useSubscription(BOOK_ADDED, {
+    onSubscriptionData: ({ subscriptionData }) => {
+      console.log(subscriptionData)
+    }
+  })
 
   useEffect(() => {
     const token = window.localStorage.getItem('library-app-token')
