e7a08da6114655ec5684fe63340f29a55891aee2
David <david.iglesias@gmail.com>
2022-03-08 10:36:35 +0100

    Part8 - library database frontend - notify subscription to user

--- part8/library-database-frontend/src/App.js
+++ part8/library-database-frontend/src/App.js
@@ -5,16 +5,18 @@ import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 import Recommend from './components/Recommend'
+import Notification from './components/Notification'
 import { BOOK_ADDED } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
   const client = useApolloClient()
 
   useSubscription(BOOK_ADDED, {
     onSubscriptionData: ({ subscriptionData }) => {
-      console.log(subscriptionData)
+      notify(`An user has added a new book titled '${subscriptionData.data.bookAdded.title}'`)
     }
   })
 
@@ -25,6 +27,13 @@ const App = () => {
     }
     setToken(token)
   }, [])
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
 
   const logout = () => {
     setToken(null)
@@ -42,6 +51,8 @@ const App = () => {
         {token && <button onClick={() => setPage('recommend')}>recommend</button>}
         {token && <button onClick={logout}>logout</button>}
       </div>
+
+      <Notification errorMessage={errorMessage} />
 
       <Authors show={page === 'authors'} token={token} />
 
