e33a979b160369ff71069d1dba79503491954375
David <david.iglesias@gmail.com>
2022-03-11 15:32:20 +0100

    Part9 - typescript - exercise5 - webbmi

---
+++ part9/bmi/bmi.ts
@@ -0,0 +1,21 @@
+export const calculateBmi = (height: number, weigth: number): string => {
+  const bmi = weigth / height / height * 100 * 100
+  switch(true) {
+    case bmi < 16:
+      return 'Underweight (Severe thinness)'
+    case bmi < 16.9:
+      return 'Underweight (Moderate thinness)'
+    case bmi < 18.4:
+      return 'Underweight (Mild thinness)'
+    case bmi < 24.9:
+      return 'Normal range'
+    case bmi < 29.9:
+      return 'Overweight (Pre-obese)'
+    case bmi < 34.9:
+      return 'Obese (Class I)'
+    case bmi < 39.9:
+      return 'Obese (Class II)'
+    default:
+      return 'Obese (Class III)'
+    }
+}
--- part9/bmi/index.ts
+++ part9/bmi/index.ts
@@ -1,8 +1,24 @@
 import express from 'express'
 const app = express()
+import { calculateBmi } from './bmi'
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!')
+})
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query
+
+  if ( !height || isNaN(Number(height)) || !weight || isNaN(Number(weight)) ) {
+    return res.send({ error: 'malformatted parameters'}).status(400)
+  }
+
+  return res.send({
+    weight,
+    height,
+    bmi: calculateBmi(Number(height), Number(weight))
+  })
+
 })
 
 const PORT = 3003
