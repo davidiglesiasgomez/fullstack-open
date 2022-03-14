21e3bc063816b13542d8b079d816c05fc7eb614d
David <david.iglesias@gmail.com>
2022-03-14 15:39:36 +0100

    Part9 - typescript - first steps - any

--- part9/first-steps/calculator.ts
+++ part9/first-steps/calculator.ts
@@ -1,28 +1,16 @@
 type Operation = 'multiply' | 'add' | 'divide';
 type Result = string | number;
 
-const calculator = (a: number, b: number, op : Operation): Result => {
+export const calculator = (a: number, b: number, op : Operation): Result => {
   switch(op) {
-    case 'multiply':
-      return a * b;
-    case 'divide':
-      if (b === 0) throw new Error('Can\'t divide by 0!');
-      return a / b;
-    case 'add':
-      return a + b;
-    default:
-      throw new Error('Operation is not multiply, add or divide!');
-    }
-}
-
-try {
-  console.log(calculator(10, 0, 'divide'))
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
+  case 'multiply':
+    return a * b;
+  case 'divide':
+    if (b === 0) throw new Error('Can\'t divide by 0!');
+    return a / b;
+  case 'add':
+    return a + b;
+  default:
+    throw new Error('Operation is not multiply, add or divide!');
   }
-  console.log(errorMessage);
-}
-
-console.log(process.env)
+};
--- part9/first-steps/index.ts
+++ part9/first-steps/index.ts
@@ -1,12 +1,30 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+const app = express();
+import { calculator } from './calculator';
 
 app.get('/ping', (_req, res) => {
-  res.send('pong')
-})
+  res.send('pong');
+});
 
-const PORT = 3003
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if ( !value1 || isNaN(Number(value1))) {
+    return res.send({ error: 'malformatted parameters'}).status(400);
+  }
+
+  if ( !value2 || isNaN(Number(value2))) {
+    return res.send({ error: 'malformatted parameters'}).status(400);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculator(Number(value1), Number(value2), op);
+  return res.send(result);
+});
+
+const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
