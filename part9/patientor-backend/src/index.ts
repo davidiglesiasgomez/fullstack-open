81b698092ab8b39dcc2e2590aea4e7ee2adc556a
davidiglesiasgomez <37771705+davidiglesiasgomez@users.noreply.github.com>
2022-04-05 15:29:23 +0200

    Part9 - typescript - exercise11 - check that works with frontend

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -3,11 +3,12 @@ import diagnoseRouter from './routes/diagnoses';
 import patientRouter from './routes/patients';
 const app = express();
 app.use(express.json());
+app.use(express.static('build'));
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
+  // console.log('someone pinged here');
   res.send('pong');
 });
 
